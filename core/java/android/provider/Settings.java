e78f9a4f965d97b034d7118142aa6b1a7adddbc1
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:34:10 +0800

    base: Incall vibration options [1/3]
    * This works with google and aosp dialer: )
    
    Change-Id: Id200af53d5a66d00ec1a420379fcff0adea1f874

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6523,6 +6523,24 @@ public final class Settings {
         public static final String FORCE_FULLSCREEN_CUTOUT_APPS = "force_full_screen_cutout_apps";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+        /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+        /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
