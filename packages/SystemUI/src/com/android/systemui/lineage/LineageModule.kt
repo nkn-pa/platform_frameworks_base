cddd64cdd21eefd9d6c54a17cfb8e5fcdafe197b
Ali B <abittin@gmail.com>
2020-10-25 18:45:10 +0300

    SystemUI: Add DerpFest Tile
    Bring it back to O
    
    Include this modifed patch:
    http://gerrit.aicp-rom.com/#/c/35017/
    
    Bring it up to Q and adapt it to the new
    OTA Updater package and activity
    
    Ported to R & S
    
    Change-Id: Icec3000dd7249001bcc65810315fe42489779466
    [nurkeinneid - Update to Android 15]
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -24,6 +24,7 @@ import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
+import com.android.systemui.qs.tiles.DerpFestTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.LocaleTile
 import com.android.systemui.qs.tiles.MusicTile
@@ -87,6 +88,12 @@ interface LineageModule {
     @IntoMap
     @StringKey(DataSwitchTile.TILE_SPEC)
     fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
+
+    /** Inject DerpFestTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DerpFestTile.TILE_SPEC)
+    fun bindDerpFestTile(derpFestTile: DerpFestTile): QSTileImpl<*>
 
     /** Inject HeadsUpTile into tileMap in QSModule */
     @Binds
