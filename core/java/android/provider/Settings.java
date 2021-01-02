b926702fbe269914740da5bfc91016f73f154614
Ali B <abittin@gmail.com>
2021-01-02 21:14:45 +0000

    base: Allow to hide arrow for back gesture [1/2]
    Thanks to Nicolas Dhouailly <ndhouailly@gmail.com>
    for the original commit for 10 in
    ChangeID Ia1465c7ce47ffa6da719e40140857b94646ac402
    
    Complete rewrite for 11 taking advantage of existing
    	 GestureNavigationSettingsObserver
    
    eyosen: Adapted to 12
    
    spkal01: Extend back gesture visibility to new affordance
    
    Change-Id: Ibf0a8083845ca1a171cb8bb8a8569765fc96e99b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12721,6 +12721,12 @@ public final class Settings {
         public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
 
         /**
+         * Whether to show or hide the arrow for back gesture
+         * @hide
+         */
+        public static final String BACK_GESTURE_ARROW = "back_gesture_arrow";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
