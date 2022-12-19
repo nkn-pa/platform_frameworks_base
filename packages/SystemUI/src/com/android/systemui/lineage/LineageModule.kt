ce3ad7f4037d496454ee23fafa43202347c4446d
maxwen <max.weninger@gmail.com>
2022-12-19 16:24:06 +0300

    SystemUI: Add Screenshot tile
        OmniROM:
    
        From: maxwen <max.weninger@gmail.com>
        Date: Wed, 14 Oct 2020 17:17:31 +0200
        Subject: [PATCH] SystemUI: qs tiles revenge
    
        -screenshot
    
        [micky387]
         * edit to the new A12 Tiles API
         * Add secondaryLabel for the long press screenshot tile
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 14 Dec 2022 13:12:26 +0100
        Subject: [PATCH] SystemUI: Update ScreenshotTile after merge
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -28,6 +28,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
+import com.android.systemui.qs.tiles.ScreenshotTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
@@ -105,6 +106,12 @@ interface LineageModule {
     @IntoMap
     @StringKey(ReadingModeTile.TILE_SPEC)
     fun bindReadingModeTile(readingModeTile: ReadingModeTile): QSTileImpl<*>
+
+    /** Inject ScreenshotTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(ScreenshotTile.TILE_SPEC)
+    fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds
