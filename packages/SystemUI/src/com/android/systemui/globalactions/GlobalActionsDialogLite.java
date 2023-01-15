caf3a98d496097723bf5f0c1151de66f263a7a3b
ezio84 <brabus84@gmail.com>
2023-01-15 16:42:03 +0100

    frameworks: Add restart systemui shortcut to power menu [1/2]
    Google made it very easy on this cycle (systemui can just kill its own pid).
    
    Change-Id: I91021e906907737c47b09da57c0181ee23cbcde7
    [nurkeinneid] partial pick of DirtyUnicorns/android_frameworks_base@9c530663650a2af9534491d94c2c7d1f52344889
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -74,6 +74,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.PowerManager;
+import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.Trace;
@@ -732,6 +733,8 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
                         && isPanicAvailable()) {
                     addIfShouldShowAction(tempActions, new PanicAction());
                 }
+            } else if (GLOBAL_ACTION_KEY_RESTART_SYSTEMUI.equals(actionKey)) {
+                addIfShouldShowAction(tempActions, new RestartSystemUIAction());
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -1491,6 +1494,27 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
             } else {
                 return false;
             }
+        }
+    }
+
+    private final class RestartSystemUIAction extends SinglePressAction {
+        private RestartSystemUIAction() {
+            super(com.android.systemui.res.R.drawable.ic_restart_systemui, com.android.systemui.res.R.string.global_action_restart_systemui);
+        }
+
+        @Override
+        public boolean showDuringKeyguard() {
+            return true;
+        }
+
+        @Override
+        public boolean showBeforeProvisioning() {
+            return true;
+        }
+
+        @Override
+        public void onPress() {
+            Process.killProcess(Process.myPid());
         }
     }
 
