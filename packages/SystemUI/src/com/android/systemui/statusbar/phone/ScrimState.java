43fe808a85127b2713c2448bb565430e5b9a41fa
nift4 <nift4@protonmail.com>
2023-04-13 09:18:41 +0200

    SystemUI: Fix scrim issues regarding transparency
    * Fix bouncer background not animating
    * Fix screen flickering when opening scrimmed bouncer
    * Fix SHADE_LOCKED ignoring custom alpha
    * Fix AOSP issue: Fix scrimmed bouncer background animating to intermediate state
    
    Change-Id: I8d4ce2a2512b6859e55f645288eb59a4d4f944b7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -88,7 +88,7 @@ public enum ScrimState {
             mBehindAlpha = mClipQsScrim ? mCustomScrimAlpha : mScrimBehindAlphaKeyguard;
             mNotifAlpha = mClipQsScrim ? mScrimBehindAlphaKeyguard : 0;
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor);
+                updateScrimColor(mScrimBehind, mCustomScrimAlpha, mBackgroundColor);
             }
         }
     },
@@ -103,7 +103,7 @@ public enum ScrimState {
             mFrontAlpha = .66f;
 
             mBehindTint = mBackgroundColor;
-            mBehindAlpha = 1f;
+            mBehindAlpha = mCustomScrimAlpha;
         }
     },
 
@@ -127,8 +127,8 @@ public enum ScrimState {
     BOUNCER {
         @Override
         public void prepare(ScrimState previousState) {
-            mBehindAlpha = mClipQsScrim ? mCustomScrimAlpha : mDefaultScrimAlpha;
-            mBehindTint = mSurfaceColor;
+            mBehindAlpha = mClipQsScrim ? 1 : mDefaultScrimAlpha;
+            mBehindTint = mBackgroundColor;
             mNotifAlpha = mClipQsScrim ? mCustomScrimAlpha : 0;
             mNotifTint = Color.TRANSPARENT;
             mFrontAlpha = 0f;
@@ -149,8 +149,12 @@ public enum ScrimState {
     BOUNCER_SCRIMMED {
         @Override
         public void prepare(ScrimState previousState) {
-            mBehindAlpha = 0;
-            mFrontAlpha = mDefaultScrimAlpha;
+            // Using previousState values makes SHADE_LOCKED -> BOUNCER_SCRIMMED smoother with no visual breakage
+            mBehindTint = previousState.mBehindTint;
+            mBehindAlpha = previousState.mBehindAlpha;
+            mFrontAlpha = 1f;
+            mNotifTint = previousState.mNotifTint;
+            mNotifAlpha = previousState.mNotifAlpha;
         }
     },
 
@@ -158,12 +162,12 @@ public enum ScrimState {
         @Override
         public void prepare(ScrimState previousState) {
             mBehindAlpha = mClipQsScrim ? mCustomScrimAlpha : mDefaultScrimAlpha;
-            mNotifAlpha = 1f;
+            mNotifAlpha = mCustomScrimAlpha;
             mFrontAlpha = 0f;
             mBehindTint = mClipQsScrim ? Color.TRANSPARENT : mBackgroundColor;
 
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor);
+                updateScrimColor(mScrimBehind, mCustomScrimAlpha, mBackgroundColor);
             }
         }
     },
@@ -273,7 +277,7 @@ public enum ScrimState {
             }
 
             if (mClipQsScrim) {
-                updateScrimColor(mScrimBehind, 1f /* alpha */, mBackgroundColor);
+                updateScrimColor(mScrimBehind, mCustomScrimAlpha, mBackgroundColor);
             }
         }
     },
