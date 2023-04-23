a699b45dde52c297170ae1944c4a62da02409dd7
SamarV-121 <samarvispute121@pm.me>
2023-04-23 13:44:56 +0530

    SystemUI: Add brightness slider styles [1/3]
    Change-Id: I0f2c05a3d0b83ba9ddaca23e08e97e87a5c69a2c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13343,6 +13343,13 @@ public final class Settings {
         public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
 
         /**
+         * Brightness slider styles
+         * @hide
+         */
+        @SuppressLint("UnflaggedApi")
+        public static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
