2e88ac719b5c2bdfb938c58e188779efc11b3de7
Pranav Vashi <neobuddy89@gmail.com>
2024-12-29 23:18:54 +0530

    Update pixel framework hooks and wrappers
    * Required by Ambient Music support
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1621,15 +1621,15 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         getNotificationShadeWindowViewController().setupCommunalHubLayout();
     }
 
-    protected NotificationPanelViewController getNotificationPanelViewController() {
+    public NotificationPanelViewController getNotificationPanelViewController() {
         return mNotificationPanelViewControllerLazy.get();
     }
 
-    protected NotificationShadeWindowViewController getNotificationShadeWindowViewController() {
+    public NotificationShadeWindowViewController getNotificationShadeWindowViewController() {
         return mNotificationShadeWindowViewControllerLazy.get();
     }
 
-    protected NotificationShadeWindowView getNotificationShadeWindowView() {
+    public NotificationShadeWindowView getNotificationShadeWindowView() {
         return getNotificationShadeWindowViewController().getView();
     }
 
@@ -3439,5 +3439,25 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         if (mCommandQueueCallbacks != null) {
             mCommandQueueCallbacks.animateExpandNotificationsPanel();
         }
+    }
+
+    @Override
+    public void startActivity(Intent intent, boolean dismissShade) {
+        mActivityStarter.startActivityDismissingKeyguard(intent, false /* onlyProvisioned */, dismissShade, null);
+    }
+
+    @Override
+    public void startPendingIntentDismissingKeyguard(PendingIntent intent) {
+        mActivityStarter.startPendingIntentDismissingKeyguard(intent);
+    }
+
+    @Override
+    public ShadeViewController getNotificationPanelViewController() {
+        return mShadeSurface;
+    }
+
+    @Override
+    public void wakeUpDeviceifDozing() {
+        mPowerInteractor.wakeUpIfDozing("AMBIENT MUSIC", PowerManager.WAKE_REASON_GESTURE);
     }
 }
