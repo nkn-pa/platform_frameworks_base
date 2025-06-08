SunOS-Project/android_sun-sdk

sdk: Allow to disable status bar notification ticker
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImplExt.java
@@ -55,6 +55,7 @@ import org.nameless.systemui.shade.CustomGestureListener;
 import org.nameless.systemui.statusbar.ticker.AdvertSwitcherView;
 import org.nameless.systemui.statusbar.ticker.MarqueeTickerEx;
 import org.nameless.systemui.statusbar.ticker.MarqueeTickerView;
+import org.nameless.systemui.statusbar.ticker.TickerController;
 import org.nameless.systemui.statusbar.ticker.TickerEx;
 
 class CentralSurfacesImplExt {
@@ -98,6 +99,7 @@ class CentralSurfacesImplExt {
     private NotificationInterruptStateProvider mNotificationInterruptStateProvider;
     private NotificationLockscreenUserManager mLockscreenUserManager;
     private StatusBarWindowController mStatusBarWindowController;
+    private TickerController mTickerController;
     private VibratorHelper mVibratorHelper;
 
     private DisplayManager mDisplayManager;
@@ -133,6 +135,7 @@ class CentralSurfacesImplExt {
             NotificationInterruptStateProvider notificationInterruptStateProvider,
             NotificationLockscreenUserManager lockscreenUserManager,
             StatusBarWindowController statusBarWindowController,
+            TickerController tickerController,
             VibratorHelper vibratorHelper) {
         mCentralSurfacesImpl = centralSurfacesImpl;
         mContext = context;
@@ -147,6 +150,7 @@ class CentralSurfacesImplExt {
         mNotifPipeline = notifPipeline;
         mNotificationInterruptStateProvider = notificationInterruptStateProvider;
         mStatusBarWindowController = statusBarWindowController;
+        mTickerController = tickerController;
         mVibratorHelper = vibratorHelper;
 
         mDisplayManager = mContext.getSystemService(DisplayManager.class);
@@ -159,6 +163,12 @@ class CentralSurfacesImplExt {
 
         mMessageRouter.subscribeTo(MSG_LONG_PRESS_BRIGHTNESS_CHANGE,
                 id -> onLongPressBrightnessChange());
+
+        mTickerController.addCallback((notificationTicker) -> {
+            if (!notificationTicker) {
+                tickerHalt();
+            }
+        });
     }
 
     void updateResources() {
@@ -281,6 +291,9 @@ class CentralSurfacesImplExt {
         mNotifCollectionListener = new NotifCollectionListener() {
             @Override
             public void onEntryAdded(@NonNull NotificationEntry entry) {
+                if (!mTickerController.showNotificationTicker()) {
+                    return;
+                }
                 if (shouldFilterHeadsUpNotification(entry)) {
                     return;
                 }
@@ -289,6 +302,9 @@ class CentralSurfacesImplExt {
 
             @Override
             public void onEntryUpdated(@NonNull NotificationEntry entry) {
+                if (!mTickerController.showNotificationTicker()) {
+                    return;
+                }
                 if (mDemoModeController.isInDemoMode()) {
                     return;
                 }

SystemUI/src/org/nameless/systemui/statusbar/ticker/TickerController.kt
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.systemui.statusbar.ticker
+
+import android.content.Context
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Handler
+import android.os.UserHandle
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Main
+import com.android.systemui.settings.UserTracker
+import com.android.systemui.util.settings.SystemSettings
+
+import java.util.concurrent.Executor
+
+import javax.inject.Inject
+
+import org.nameless.provider.SettingsExt.System.STATUS_BAR_NOTIFICATION_TICKER
+
+@SysUISingleton
+class TickerController @Inject constructor(
+    private val context: Context,
+    @Main private val mainExecutor: Executor,
+    @Main mainHandler: Handler,
+    private val systemSettings: SystemSettings,
+    private val userTracker: UserTracker
+) {
+
+    private val callbacks = mutableListOf<Callback>()
+
+    private var notificationTicker = true
+
+    init {
+        val settingsObserver = object : ContentObserver(mainHandler) {
+            override fun onChange(selfChange: Boolean, uri: Uri?) {
+                when (uri?.lastPathSegment) {
+                    STATUS_BAR_NOTIFICATION_TICKER -> updateNotificationTicker(true)
+                }
+            }
+        }
+        systemSettings.registerContentObserverForUserSync(
+                STATUS_BAR_NOTIFICATION_TICKER,
+                settingsObserver, UserHandle.USER_ALL)
+        updateSettings()
+
+        userTracker.addCallback(object : UserTracker.Callback {
+            override fun onUserChanged(newUser: Int, userContext: Context) {
+                updateSettings()
+            }
+        }, mainExecutor)
+    }
+
+    private fun updateSettings() {
+        updateNotificationTicker(false)
+        notifySettingsChanged()
+    }
+
+    private fun updateNotificationTicker(notifyChange: Boolean) {
+        notificationTicker = systemSettings.getIntForUser(STATUS_BAR_NOTIFICATION_TICKER,
+                1, userTracker.userId) == 1
+        if (notifyChange) {
+            notifySettingsChanged()
+        }
+    }
+
+    fun showNotificationTicker() = notificationTicker
+
+    fun addCallback(callback: Callback) {
+        callbacks.add(callback)
+    }
+
+    fun removeCallback(callback: Callback) {
+        callbacks.remove(callback)
+    }
+
+    private fun notifySettingsChanged() {
+        callbacks.forEach {
+            it.onSettingsChanged(
+                notificationTicker
+            )
+        }
+    }
+
+    interface Callback {
+        fun onSettingsChanged(
+            notificationTicker: Boolean
+        )
+    }
+}

nkn-pa/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImplExt.java
@@ -32,6 +32,7 @@ import com.android.systemui.statusbar.window.StatusBarWindowController;
 import org.sun.systemui.statusbar.ticker.AdvertSwitcherView;
 import org.sun.systemui.statusbar.ticker.MarqueeTickerEx;
 import org.sun.systemui.statusbar.ticker.MarqueeTickerView;
+import org.sun.systemui.statusbar.ticker.TickerController;
 import org.sun.systemui.statusbar.ticker.TickerEx;
 
 class CentralSurfacesImplExt {
@@ -58,6 +59,7 @@ class CentralSurfacesImplExt {
     private NotificationInterruptStateProvider mNotificationInterruptStateProvider;
     private NotificationLockscreenUserManager mLockscreenUserManager;
     private StatusBarWindowController mStatusBarWindowController;
+    private TickerController mTickerController;
 
     private AdvertSwitcherView mSwitcherView;
     private MarqueeTickerEx mTicker;
@@ -71,7 +73,8 @@ class CentralSurfacesImplExt {
             NotifPipeline notifPipeline,
             NotificationInterruptStateProvider notificationInterruptStateProvider,
             NotificationLockscreenUserManager lockscreenUserManager,
-            StatusBarWindowController statusBarWindowController) {
+            StatusBarWindowController statusBarWindowController,
+            TickerController tickerController) {
         mCentralSurfacesImpl = centralSurfacesImpl;
         mContext = context;
         mDemoModeController = demoModeController;
@@ -82,12 +85,16 @@ class CentralSurfacesImplExt {
         mNotifPipeline = notifPipeline;
         mNotificationInterruptStateProvider = notificationInterruptStateProvider;
         mStatusBarWindowController = statusBarWindowController;
+        mTickerController = tickerController;
     }
 
     private void initEntryListener() {
         mNotifCollectionListener = new NotifCollectionListener() {
             @Override
             public void onEntryAdded(@NonNull NotificationEntry entry) {
+                if (!mTickerController.showNotificationTicker()) {
+                    return;
+                }
                 if (shouldFilterHeadsUpNotification(entry)) {
                     return;
                 }
@@ -96,6 +103,9 @@ class CentralSurfacesImplExt {
 
             @Override
             public void onEntryUpdated(@NonNull NotificationEntry entry) {
+                if (!mTickerController.showNotificationTicker()) {
+                    return;
+                }
                 if (mDemoModeController.isInDemoMode()) {
                     return;
                 }

packages/SystemUI/src/org/sun/systemui/statusbar/ticker/TickerController.kt
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.sun.systemui.statusbar.ticker
+
+import android.content.Context
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Handler
+import android.os.UserHandle
+import android.provider.Settings
+
+import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Main
+import com.android.systemui.settings.UserTracker
+import com.android.systemui.util.settings.SystemSettings
+
+import java.util.concurrent.Executor
+
+import javax.inject.Inject
+
+@SysUISingleton
+class TickerController @Inject constructor(
+    private val context: Context,
+    @Main private val mainExecutor: Executor,
+    @Main mainHandler: Handler,
+    private val systemSettings: SystemSettings,
+    private val userTracker: UserTracker
+) {
+
+    private val callbacks = mutableListOf<Callback>()
+
+    private var notificationTicker = true
+
+    init {
+        val settingsObserver = object : ContentObserver(mainHandler) {
+            override fun onChange(selfChange: Boolean, uri: Uri?) {
+                when (uri?.lastPathSegment) {
+                    STATUS_BAR_NOTIFICATION_TICKER -> updateNotificationTicker(true)
+                }
+            }
+        }
+        systemSettings.registerContentObserverForUserSync(
+                Settings.System.STATUS_BAR_NOTIFICATION_TICKER,
+                settingsObserver, UserHandle.USER_ALL)
+        updateSettings()
+
+        userTracker.addCallback(object : UserTracker.Callback {
+            override fun onUserChanged(newUser: Int, userContext: Context) {
+                updateSettings()
+            }
+        }, mainExecutor)
+    }
+
+    private fun updateSettings() {
+        updateNotificationTicker(false)
+        notifySettingsChanged()
+    }
+
+    private fun updateNotificationTicker(notifyChange: Boolean) {
+        notificationTicker = systemSettings.getIntForUser(STATUS_BAR_NOTIFICATION_TICKER,
+                1, userTracker.userId) == 1
+        if (notifyChange) {
+            notifySettingsChanged()
+        }
+    }
+
+    fun showNotificationTicker() = notificationTicker
+
+    fun addCallback(callback: Callback) {
+        callbacks.add(callback)
+    }
+
+    fun removeCallback(callback: Callback) {
+        callbacks.remove(callback)
+    }
+
+    private fun notifySettingsChanged() {
+        callbacks.forEach {
+            it.onSettingsChanged(
+                notificationTicker
+            )
+        }
+    }
+
+    interface Callback {
+        fun onSettingsChanged(
+            notificationTicker: Boolean
+        )
+    }
+}