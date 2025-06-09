b3eb1887722a8b16e729f02e3c6889ed9b72a2e1
NurKeinNeid <nurkeinneid@derpfest.org>
2025-06-09 19:38:29 +0200

    SystemUI: Add null checks in HeadsUpAppearanceController for view handling

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -308,6 +308,12 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
      *
      */
     private void hide(View view, int endState, Runnable callback) {
+        if (view == null) {
+            if (callback != null) {
+                callback.run();
+            }
+            return;
+        }
         if (mAnimationsEnabled) {
             CrossFadeHelper.fadeOut(view, CONTENT_FADE_DURATION /* duration */,
                     0 /* delay */, () -> {
@@ -325,6 +331,9 @@ public class HeadsUpAppearanceController extends ViewController<HeadsUpStatusBar
     }
 
     private void show(View view) {
+        if (view == null) {
+            return;
+        }
         if (mAnimationsEnabled) {
             CrossFadeHelper.fadeIn(view, CONTENT_FADE_DURATION /* duration */,
                     CONTENT_FADE_DELAY /* delay */);
